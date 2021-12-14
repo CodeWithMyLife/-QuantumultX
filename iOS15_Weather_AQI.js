4fbc61ea73a3e1bdcf3211de0520cde74cf31b30
CodeWithMyLife <codewithmylife@gmail.com>
2021-12-14 21:00:58 +0800

    Create iOS15_Weather_AQI.js

---
+++ iOS15_Weather_AQI.js
@@ -0,0 +1,3 @@
+# > iOS15 Weather AQI 修正
+^https:\/\/weather-data\.apple\.com\/v2\/weather\/[\w-]+\/-?[0-9]+\.[0-9]+\/-?[0-9]+\.[0-9]+\? url script-response-body iOS15_Weather_AQI.js
+hostname = weather-data.apple.com
